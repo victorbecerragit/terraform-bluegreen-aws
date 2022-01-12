c1863481de91bf3e64dba03c8b84506c2c367e16
vbecerra <victor.becerra.ext@nokia.com>
2022-01-12 10:14:05 +0100

    initial commit main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
-# required file
+provider "aws" {
+ region  = "us-west-2"
+}
+
+variable "production" {
+ default = "green"
+}
+
+module "base" {
+ source     = "./modules/base"
+ production = var.production
+}
+
+module "green" {
+ source      = "./modules/autoscaling"
+ app_version = "v1.0"
+ label       = "green"
+ base        = module.base
+}
+
+module "blue" {
+ source      = "./modules/autoscaling"
+ app_version = "v2.0"
+ label       = "blue"
+ base        = module.base
+}
+
+output "lb_dns_name" {
+ value = module.base.lb_dns_name
+}
